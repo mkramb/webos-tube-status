80e10b8f46eae3b74dbb2afc980027169088a397
Mitja Kramberger <mitja.kramberger@gmail.com>
2017-11-26 21:37:41 +0000

    Adding custom API call

--- src/reducers/items.js
+++ src/reducers/items.js
@@ -3,10 +3,7 @@ import { ITEMS_FETCH_COMPLETE } from '../actions'
 export const items = (state = [], action) => {
   switch (action.type) {
     case ITEMS_FETCH_COMPLETE:
-      return action.payload.map((item) => ({
-        name: item.name,
-        status: item.lineStatuses[0].statusSeverityDescription        
-      }))
+      return action.payload;
 
     default:
       return state
--- src/services/status-items.js
+++ src/services/status-items.js
@@ -1,4 +1,4 @@
-const API_STATUS_ALL = 'https://api.tfl.gov.uk/line/mode/tube,overground,dlr,tflrail/status'
+const API_STATUS_ALL = 'https://ip9datgv1l.execute-api.eu-west-2.amazonaws.com/dev/status'
 
 export const getStatusItems = () => 
   fetch(API_STATUS_ALL)
